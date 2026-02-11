ff0dd981878cc1137e8c9f9bc38d578acdd5b36b
Julian Espinoza <julianespi2002@gmail.com>
2026-02-11 11:27:34 -0800

    cleaned up solution to problem 4

--- Project 4/Main.cpp
+++ Project 4/Main.cpp
@@ -10,8 +10,11 @@ void main4();
 int main()
 {
 	main1();
-	main2();
-	main3();
+
+	//main2();
+	//main3();
+
+	cout << endl << endl << "Problem 4" << endl;
 	main4();
 
 	return 0;
@@ -25,7 +28,7 @@ void main1()
 	char kindOfDrink;
 	int sandwiches;
 	int sandwichesSize;
-	int bill;
+	int bill =0;
 	
 	cout << "----------------------7-11 Convenient Store ----------------------" << endl;
 	cout << "Drinks" << endl;
@@ -56,49 +59,47 @@ void main4()
 	short sizeOfRam =0;
 	_asm 
 	{
-		//00 = 0, 01, = 1, 10, =2
 		// number of printers
-		mov ax, 1100111010011100b;
-		mov bx, 1100000000000000b;
-		and ax, bx;
-		shr ax, 14;
-		mov numOfPrinters, ax;
+		mov ax, 1100111010011100b;	//ax = 1100 1110 1001 1100
+		mov bx, 1100000000000000b;	//bx = 1100 0000 0000 0000
+		and ax, bx;					//ax = 1100 1110 1001 1100 AND 1100 0000 0000 0000
+		shr ax, 14;					//ax = 1100 0000 0000 0000 shifted to 0000 0000 0000 0011
+		mov numOfPrinters, ax;		//numOfPrinters = 0000 0000 0000 0011 = 3
 
 
 
 		//number of floppy drives
-		mov ax, 1100111010011100b;
-		mov bx, 0000000011000000b;
-		and ax, bx;
-		shr ax, 6;
-		inc ax;
-		mov numOfFloppyDrives, ax;
+		mov ax, 1100111010011100b;	//ax = 1100 1110 1001 1100
+		mov bx, 0000000011000000b;	//bx = 0000 0000 1100 0000
+		and ax, bx;					//ax = 1100 1110 1001 1100 AND 0000 0000 1100 0000
+		shr ax, 6;					//ax = 0000 0000 1000 0000 shifted to 0000 0000 0000 0010
+		inc ax;						//ax = 0000 0000 0000 0010 + 1 = 2 + 1; 
+		mov numOfFloppyDrives, ax;	//numOfFloppyDirves = ax
 
-		//add increments of 16
 		//size of ram
-		mov ax, 1100111010011100b;
-		mov bx, 0000000000001100b;
-		and ax, bx;
-		shr ax, 2;
-		mov cx, 0;
-		mov bx, 16;
-		forLoop: //using for loop;
-			cmp cx, ax;
-			je done;
+		mov ax, 1100111010011100b;	//ax = 1100 1110 1001 1100
+		mov bx, 0000000000001100b;	//bx = 0000 0000 0000 1100
+		and ax, bx;					//ax = 1100 1110 1001 1100 AND 0000 0000 0000 1100
+		shr ax, 2;					//ax = 0000 0000 0000 1100 shifted to 0000 0000 0000 0011
+		mov cx, 0;					//cx = 0 will be used as loop counter
+		mov bx, 16;					//bx will hold the sum of ram
+		forLoop:					//using for loop;
+			cmp cx, ax;				//compare cx (the counter) to ax the limit for the loop
+			je done;				// if cs == ax break loop
 
-			inc cx;
-			add bx, 16;
-			jmp forLoop;
+			inc cx;					//cx++
+			add bx, 16;				//bx += 16
+			jmp forLoop;			//restarts the for loop
 
-		done:
+		done:						//break
 			
-		mov sizeOfRam, bx;
+		mov sizeOfRam, bx;			//sizeOfRam = bx
 
 		
 	}
 
-	cout << numOfPrinters << endl;
-	cout << numOfFloppyDrives << endl;
-	cout << sizeOfRam << endl;
+	cout << "The number of printers connected to the computer: " << numOfPrinters << endl;
+	cout << "The number of floppy drives: " << numOfFloppyDrives << endl;
+	cout << "The size of the RAM: " << sizeOfRam << " GB RAM" << endl;
 	
 }
