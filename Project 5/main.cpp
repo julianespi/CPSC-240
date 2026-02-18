c6bf640b44e585d4899f21d31fb85e0fdb409eec
Julian Espinoza <julianespi2002@gmail.com>
2026-02-18 09:23:25 -0800

    start of project 5

---
+++ Project 5/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void main1();
+void main2();
+void main3();
+
+int main()
+{
+	main1();
+	main2();
+	main3();
+
+	return 0;
+}
+
+void main1() 
+{
+
+}
+
+void main2()
+{
+
+}
+
+void main3()
+{
+
+}
